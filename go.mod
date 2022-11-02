0121ef37cba40403507f636213775a77b82677fe
Stefan Vlajkovic <vlajkovicstefan01@gmail.com>
2022-11-02 11:27:57 +0100

    Start initialization of cassandra db

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module tweet
 go 1.19
 
 require (
+	github.com/gocql/gocql v1.2.1
 	github.com/gorilla/mux v1.8.0
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/uber/jaeger-client-go v2.30.0+incompatible
@@ -11,7 +12,10 @@ require (
 
 require (
 	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 )
--- repository/cassandra/cassandra_tweet_repository.go
+++ repository/cassandra/cassandra_tweet_repository.go
@@ -1,8 +1,30 @@
 package cassandra
 
+import (
+	"github.com/gocql/gocql"
+)
+
 type CassandraTweetRepository struct {
+	session *gocql.Session
 }
 
 func NewCassandraTweetRepository() (*CassandraTweetRepository, error) {
-	return nil, nil
+	//dbport := os.Getenv("DBPORT")
+	//db := os.Getenv("DB")
+	//host := fmt.Sprintf("%s:%s", db, dbport)
+
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.ProtoVersion = 4
+	cluster.Keyspace = "tweet_database"
+	cluster.Consistency = gocql.Quorum
+
+	session, err := cluster.CreateSession()
+	//defer session.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return &CassandraTweetRepository{
+		session: session,
+	}, nil
 }
